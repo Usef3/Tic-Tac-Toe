a3a31289547f371d578a0b31a3ca12291dd12c31
Yousef Elsheikh <148818921+Usef3@users.noreply.github.com>
2024-08-07 10:07:15 +0300

    Add files via upload

---
+++ Tic Tac Toe.py
@@ -0,0 +1,9 @@
+class Name:
+    def __init__(self):
+        self.player=""
+        self.symbol=""
+    def choose_name(self):
+        
+        name=input("Enter your name (letters only) : ")
+
+        
