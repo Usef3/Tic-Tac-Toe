2bd3250bb86752e5430f7dc27f01a366fca1e6a4
Yousef Elsheikh <148818921+Usef3@users.noreply.github.com>
2024-08-08 13:27:42 +0300

    Update Tic Tac Toe.py

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -1,7 +1,7 @@
 class Name:
     def __init__(self):
         self.player=""
-        self.symbol=""
+        self.symbol="" 
     def choose_name(self):
         
         name=input("Enter your name (letters only) : ")
