9e4714bd3d301a4d1697fba33ad1ab3a6609dd19
Yousef Elsheikh <148818921+Usef3@users.noreply.github.com>
2024-08-18 18:42:12 +0300

    Delete Tic Tac Toe.py

--- Tic Tac Toe.py
+++
@@ -1,9 +0,0 @@
-class Name:
-    def __init__(self):
-        self.player=""
-        self.symbol="" 
-    def choose_name(self):
-        
-        name=input("Enter your name (letters only) : ")
-
-        
